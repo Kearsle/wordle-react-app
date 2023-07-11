ded657971a98747958dd2acd59721527941a873d
DKearsleyBrown <97604238+DKearsleyBrown@users.noreply.github.com>
2023-07-11 17:36:20 +0100

    Login and Register Page init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navigation';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Wordle</h1>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact Component={Home}></Route>
+        <Route path='/login' exact Component={Login}></Route>
+        <Route path='/register' exact Component={Register}></Route>
+      </Routes>
+    </Router>
   );
 }
 
