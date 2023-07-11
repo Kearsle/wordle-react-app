58e1d0055efea2867094a42cb99c31acc03ac4db
Daniel Kearsley-Brown <danielkearsleybrown@gmail.com>
2023-07-11 22:47:33 +0100

    Updated Login Section+

--- src/components/LoginSection.js
+++ src/components/LoginSection.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSection.css";
 import { Link } from "react-router-dom"
 
 function LoginSection() {
+  const [username, setUsername] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username);
+  }
+
   return (
     <div class="login-container">
       <div id="login" className="login-card">
-        <h1>Login</h1>
-        <label>Username: </label>
-        <input type="text" name="wordleUsername" />
-        <label>Password: </label>
-        <input type="password" name="wordlePassword" />
-        <button>Login</button>
+        <form onSubmit={handleSubmit}>
+          <label for="username">Username:</label>
+          <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" id="username" name="username" />
+          <label for="password">Password:</label>
+          <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
+          <button type="submit">Login</button>
+        </form>
         <div className="register-container">
           <Link to='/register'>Register</Link>
         </div>
