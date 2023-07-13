49431f6006a90036db02c4350248372d6eb2e654
Daniel Kearsley-Brown <danielkearsleybrown@gmail.com>
2023-07-13 14:12:01 +0100

    Form Inputs Alteration (Incomplete)

---
+++ src/components/FormInput.js
@@ -0,0 +1,11 @@
+import "./FormInput.css";
+
+function FormInput(props) {
+    return (
+        <div className="formInput">
+            <input name={props.name} placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default FormInput
--- src/components/LoginSection.js
+++ src/components/LoginSection.js
@@ -1,31 +1,50 @@
 import React, { useState } from "react";
 import "./LoginSection.css";
 import { Link } from "react-router-dom"
+import FormInput from "./FormInput";
 import axios from "axios";
 
 function LoginSection() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [values, setvalues] = useState({
+    username:"",
+    password:""
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username"
+    },
+    {
+      id: 2,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password"
+    }
+]
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    axios.post('http://127.0.0.1:8000/user/login', {username, password})
+    const data = new FormData(e.target)
+    console.log(Object.fromEntries(data.entries()))
+    /*axios.post('http://127.0.0.1:8000/user/login', {username, password})
       .then(res => {
         console.log(res.data.userID)
         setUsername("")
         setPassword("")
         })
-      .catch(err => console.log(err.response.data.error));
+      .catch(err => console.log(err.response.data.error));*/
   }
 
   return (
     <div class="login-container">
       <div id="login" className="login-card">
         <form className="login-form" action="POST" onSubmit={handleSubmit}>
-          <div className="input">
-            <label for="username">Username:</label>
-            <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" id="username" name="username" />
-          </div>
+          <FormInput name="username" placeholder="Username"/>
           <div className="input">
             <label for="password">Password:</label>
             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" />
